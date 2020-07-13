8c8b6c047abfecc77c960c3670803a018ad57ab9
veronicamars73 <mendie73@gmail.com>
2020-07-13 10:29:13 -0300

    Adding aula 11

---
+++ aula-11/include/App.h
@@ -0,0 +1,26 @@
+#ifndef W5N_APP_H
+#define W5N_APP_H
+
+#include "Diary.h"
+#include "Message.h"
+
+
+#include <string>
+#include <vector>
+
+struct App
+{
+    Diary diary;
+
+    App(const std::string& filename);
+    int run(int argc, char* argv[]);
+    
+    void add();
+    void add(const std::string message);
+    std::vector<Message*> search();
+    std::vector<Message*> search(const std::string looked);
+    void list_messages();
+    void print_message(Message message);
+    void result_search(std::vector<Message*> pointer);
+}; 
+#endif
