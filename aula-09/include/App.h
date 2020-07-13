28c19a0612c3b6d391d6a75d778f734e7bf8ee58
veronicamars73 <mendie73@gmail.com>
2020-07-13 10:27:53 -0300

    Adding aula 9

---
+++ aula-09/include/App.h
@@ -0,0 +1,20 @@
+#ifndef W5N_APP_H
+#define W5N_APP_H
+
+#include "Diary.h"
+
+
+#include <string>
+
+struct App
+{
+    Diary diary;
+
+    App(const std::string& filename);
+    int run(int argc, char* argv[]);
+    
+    void add();
+    void add(const std::string message);
+    void list_messages();
+}; 
+#endif
---
+++ aula-09/src/App.cpp
@@ -0,0 +1,62 @@
+#include "../include/App.h"
+#include "../include/Date.h"
+#include "../include/Time.h"
+#include "../include/Helpers.h"
+#include <iostream>
+#include <string>
+
+App::App(const std::string& filename) : diary(filename)
+{
+}
+
+int App::run(int argc, char* argv[])
+{
+    if (argc == 1) {
+        return show_usage(argv[0]);
+    }
+    // For Testing
+    // for (int i = 0; i < 10; ++i){
+    //     add();
+    // }
+    // list_messages();
+    // Ending test
+    std::string action = argv[1];
+
+    if (action == "add") {
+        if (argc == 2) {
+            add();
+        } else {
+            add(argv[2]);
+        }
+    } else if (action == "list") {
+        list_messages();
+    } else if (action == "search") {
+    } else {
+        return show_usage(argv[0]);
+    }
+
+    return 0;
+}
+
+void App::add()
+{
+    std::string message;
+    std::cout << "Enter your message:" << std::endl;
+    std::getline(std::cin, message);
+
+    add(message);
+}
+
+void App::add(const std::string message)
+{
+    diary.add(message);
+    
+}
+
+void App::list_messages()
+{
+    for (size_t i = 0; i < diary.message_size; ++i) {
+        Message message = diary.messages[i];
+        std::cout << "- " << message.date.get_date_string() << " " << message.time.get_time_string() << " " << message.content << std::endl;
+    }
+}
