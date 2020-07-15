b441773ed460474683a206c2c8bc05b7c74569ac
veronicamars73 <mendie73@gmail.com>
2020-07-15 11:17:17 -0300

    Adding Fila class

--- aula-17/src/main.cpp
+++ aula-17/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Cliente.hpp"
 #include "ClientePJ.hpp"
 #include "Agencia.hpp"
+#include "Fila.hpp"
 
 
 #include <iostream>
@@ -13,6 +14,13 @@ int main(int argc, char* argv[]) {
     Agencia a2("Agencia 2", 2084);
     Cliente c1("092.127.434-44", "Jane Doe");
     Cliente c2("092.127.435-44", "Poor Jane Doe");
+    Cliente c3("092.127.453-44", "john doe");
+    Cliente c4("092.009.453-43", "another john doe");
+    ClientePJ cj1("092.127.987-44", "Jane Doe Empresa");
+    ClientePJ cj2("092.127.987-44", "Poor Jane Doe Empresa");
+    ClientePJ cj3("092.127.987-44", "john doe Empresa");
+    ClientePJ cj4("092.009.987-43", "another john doe Empresa");
+
     Conta conta_um(1233, 50000.00, c1, a1);
     Conta conta_dois(1345, 200.00, c2, a2);
     std::cout << "Conta de " << conta_um.titular.nome << " " << conta_um.agenciaDaConta.nome << " " << conta_um.numero << ":" << conta_um.saldo << std::endl;
@@ -36,4 +44,26 @@ int main(int argc, char* argv[]) {
     std::cout << "Conta de " << conta_um.titular.nome << " " << conta_um.agenciaDaConta.nome << " " << conta_um.numero << ":" << conta_um.saldo << std::endl;
     std::cout << "Conta de " << conta_dois.titular.nome << " " << conta_dois.agenciaDaConta.nome << " " << conta_dois.numero << ":" << conta_dois.saldo << std::endl;
     std::cout << "A quantidade de contas é " << Conta::qtd_contas << std::endl;
+
+    Fila<Cliente> clientes(3);
+    clientes.add(c1);
+    clientes.add(c2);
+    clientes.add(c3);
+    clientes.add(c4);
+    clientes.remove();
+    clientes.remove();
+    clientes.remove();
+    clientes.remove();
+    clientes.isEmpty();
+
+    Fila<ClientePJ> clientesPJ(3);
+    clientesPJ.add(cj1);
+    clientesPJ.add(cj2);
+    clientesPJ.add(cj3);
+    clientesPJ.add(cj4);
+    clientesPJ.remove();
+    clientesPJ.remove();
+    clientesPJ.remove();
+    clientesPJ.remove();
+    clientesPJ.isEmpty();
 }
