dc5576aae6ff11fccd6edcdf1029bc88d16bf4a5
veronicamars73 <mendie73@gmail.com>
2020-07-15 10:36:39 -0300

    Adding banco from aula 15

---
+++ aula-17/include/Conta.hpp
@@ -0,0 +1,25 @@
+#ifndef W5N_CONTA_H
+#define W5N_CONTA_H
+
+#include <string>
+#include "Cliente.hpp"
+#include "Agencia.hpp"
+
+class Conta{
+    public:
+    int numero;
+    Cliente titular;
+    Agencia agenciaDaConta;
+    double saldo;
+
+    static int qtd_contas;
+
+    Conta();
+    Conta(int numero, double saldo, Cliente titular, Agencia agenciaDaConta);
+
+    void saca(double valor);
+    void deposita(double valor);
+    void transfere(double valor, Conta& conta_recebe);
+};
+
+#endif
